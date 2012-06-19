e3f2f1ec85f63b911da04c1b96a9cb6a94e3fbd6
Antoine Sabot-Durand <antoine@sabot-durand.net>
2012-06-19 15:47:30 +0200

    Correcting AGOVA-10

--- src/main/java/org/agorava/socializer/HubProducer.java
+++ src/main/java/org/agorava/socializer/HubProducer.java
@@ -34,7 +34,7 @@ public class HubProducer {
 
     @Twitter
     @ApplicationScoped
-    @OAuthApplication(apiKey = "FQzlQC49UhvbMZoxUIvHTQ", apiSecret = "VQ5CZHG4qUoAkUUmckPn4iN4yyjBKcORTW0wnok4r1k", callback = "/callback.jsf")
+    @OAuthApplication(apiKey = "${twitter.key}", apiSecret = "${twitter.secret}", callback = "${callback}")
     @Produces
     public TwitterServicesHub twitterProducer(TwitterServicesHub service) {
         return service;
@@ -42,7 +42,7 @@ public class HubProducer {
 
     @LinkedIn
     @ApplicationScoped
-    @OAuthApplication(apiKey = "ympq1JR_oxeC3qZE4VwiDEr-9Rc9Am0YE1AJwwXJNREfqaF7J6hXfsncu_JFd13W", apiSecret = "RwDk21M6qQeGT_zi2icmZV6tc5VsjRZPm7DWDIVt0Wsqu2eYBXt4Csg-FUbBZIIH", callback = "/callback.jsf")
+    @OAuthApplication(apiKey = "${linkedin.key}", apiSecret = "${linkedin.secret}", callback = "${callback}")
     @Produces
     public LinkedInServicesHub linkedInProducer(LinkedInServicesHub service) {
         return service;
@@ -50,7 +50,7 @@ public class HubProducer {
 
     @Facebook
     @ApplicationScoped
-    @OAuthApplication(apiKey = "204631749555557", apiSecret = "5d3132b1448a66d28e5c420b267cd65e", callback = "/callback.jsf", scope = "read_stream publish_stream")
+    @OAuthApplication(apiKey = "${facebook.key}", apiSecret = "${facebook.secret}", callback = "${callback}", scope = "read_stream publish_stream")
     @Produces
     public FacebookServicesHub facebookProducer(FacebookServicesHub service) {
         return service;
